8d6834d0f76576667cc61a9eadf56165018cb225
Joshua <jleaton.me@gmail.com>
2021-11-22 11:51:26 -0600

    adds pkg layer for reusable functionality not application specific

---
+++ pkg/connect.go
@@ -0,0 +1,50 @@
+package pkg
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+//handles the DB connection logic with the use of some helper functions connectionStringFormat() and driverConnect()
+func DBConnect(username, password, host, dbName, sslMode string) (*sql.DB, error) {
+
+	connectionString, err := connectionStringFormat(username, password, host, dbName, sslMode)
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := driverConnect(connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	//ensure database connection is still active
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectionStringFormat(username, password, host, dbName, sslMode string) (string, error) {
+	switch true {
+	case username == "" || password == "" || host == "" || dbName == "" || sslMode == "":
+		return "", fmt.Errorf("all parameters are required to have at least one character to DBConnect")
+	case strings.Contains(username, " ") || strings.Contains(password, " ") || strings.Contains(host, " ") || strings.Contains(dbName, " ") || strings.Contains(sslMode, " "):
+		return "", fmt.Errorf("can not have any space in parameters passed to DBConnect")
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=%s", username, password, host, dbName, sslMode), nil
+}
+
+//connects to postgres driver with given connection string
+func driverConnect(connectionStr string) (*sql.DB, error) {
+
+	db, err := sql.Open("postgres", connectionStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
